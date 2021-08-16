75a257d855ba5ce950b2b9805f265cbe691f47bd
LuisFelipeCruz <luisfelipecruzgeo@hotmail.com>
2021-08-16 13:59:03 -0500

    feat(basesProyect): bases funcionales del proyecto para comenzar hacar las funcionadades correspondientes, ajustar application properties con sus datos de concexion a la db en MySQL para que les funcione bien

---
+++ src/main/java/com/productos/Dw3Grupo4EcommerceMs2Application.java
@@ -0,0 +1,15 @@
+package com.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class Dw3Grupo4EcommerceMs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dw3Grupo4EcommerceMs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/productos/Dw3Grupo4EcommerceMs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dw3Grupo4EcommerceMs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
